4a2093e610b0d23ed81d801e2ca39dd327c5518b
TejaSree03 <aruvatejasree@gmail.com>
2025-04-24 09:20:59 +0000

    property 3nd commit

--- springapp/src/main/java/com/examly/springapp/exceptions/PropertyInquiryException.java
+++
@@ -1,8 +0,0 @@
-package com.examly.springapp.exceptions;
-public class PropertyInquiryException extends RuntimeException {
-    
-    public PropertyInquiryException(String msg) {
-        super(msg);
-    }
-
-}
