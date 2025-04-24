149158f754a0194236e362d9663a5cb75f16445c
jaskaransingh16 <jaskaransinghbasra4@gmail.com>
2025-04-24 07:14:49 +0000

    Created login , auth controller as well as security config

--- springapp/src/main/java/com/examly/springapp/model/LoginDTO.java
+++ springapp/src/main/java/com/examly/springapp/model/LoginDTO.java
@@ -2,4 +2,5 @@ package com.examly.springapp.model;
 
 public class LoginDTO {
 
+    
 }
--- springapp/src/main/java/com/examly/springapp/repository/UserRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepo.java
@@ -1,9 +1,13 @@
 package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.examly.springapp.model.User;
 
 public interface UserRepo extends JpaRepository<User,Long>{
+
+    @Query("select user from User user where user.email=?1")
+    User findByEmail(String email);
     
 }
