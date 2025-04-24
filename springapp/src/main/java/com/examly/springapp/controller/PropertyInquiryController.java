b7a4c077633369893978ed0dfc1e04faf576bdd9
Dinesh12345-ai <mr.dhinesh9010@gmail.com>
2025-04-24 06:10:03 +0000

    PropertyInquiry Controller

--- springapp/src/main/java/com/examly/springapp/controller/PropertyInquiryController.java
+++ springapp/src/main/java/com/examly/springapp/controller/PropertyInquiryController.java
@@ -1,5 +1,77 @@
 package com.examly.springapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.model.PropertyInquiry;
+import com.examly.springapp.service.PropertyInquiryService;
+
+@RestController
+@RequestMapping("/api/inquiries")
 public class PropertyInquiryController {
-    
+@Autowired
+PropertyInquiryService  propertyInquiryService;
+@PostMapping
+public ResponseEntity<PropertyInquiry> addInquiry(@RequestBody PropertyInquiry propertyInquiry){
+    propertyInquiry = propertyInquiryService.addInquiry(propertyInquiry);
+    return ResponseEntity.status(201).body(propertyInquiry);
+}
+
+@GetMapping("/{inquiryId}")
+public ResponseEntity<?> getInquiryById(@PathVariable long inquiryId){
+    PropertyInquiry propertyInquiry = propertyInquiryService.getInquiryById(inquiryId);
+    if(propertyInquiry==null){
+    return ResponseEntity.status(200).body(propertyInquiry);
+    }
+    return ResponseEntity.status(404).body("Inquiry not found!");
+}
+
+@GetMapping("/user/{userId}")
+public ResponseEntity<?> getInquiriesByUser(@PathVariable long userId){
+    List<PropertyInquiry> propertyInquiriesList = propertyInquiryService.getInquiriesByUser(userId);
+    if(propertyInquiriesList.size()!=0){
+    return ResponseEntity.status(200).body(propertyInquiriesList);
+    }
+    return ResponseEntity.status(404).body("No Inquiries found!");
+}
+
+@GetMapping("")
+public ResponseEntity<?> getAllInquiries(){
+    List<PropertyInquiry> propertyInquiriesList = propertyInquiryService.getAllInquiries();
+    if(propertyInquiriesList.size()!=0){
+    return ResponseEntity.status(200).body(propertyInquiriesList);
+    }
+    return ResponseEntity.status(404).body("No Inquiries found!");
+}
+
+
+@PutMapping("/{inquiryId}")
+public ResponseEntity<?> updateInquiryById(@PathVariable long inquiryId){
+    PropertyInquiry propertyInquiry = propertyInquiryService.updateInquiryById(inquiryId);
+    if(propertyInquiry==null){
+        return ResponseEntity.status(200).body(propertyInquiry);
+    }
+        return ResponseEntity.status(404).body("Update is Unsuccessful!");
+}
+
+@DeleteMapping("/{inquiryId}")
+public ResponseEntity<String> deleteInquiryById(@PathVariable long inquiryId){
+    boolean flag = propertyInquiryService.deleteInquiryById(inquiryId);
+    if(flag){
+        return ResponseEntity.status(200).body("Deletion is successfull!")
+    }
+    return ResponseEntity.status(404).body("Deletion is unsuccessfull!")
+}
+
+
+
 }
--- springapp/src/main/java/com/examly/springapp/service/PropertyInquiryService.java
+++ springapp/src/main/java/com/examly/springapp/service/PropertyInquiryService.java
@@ -1,3 +1,34 @@
+package com.examly.springapp.service;
+
+import java.util.List;
+
+import com.examly.springapp.model.PropertyInquiry;
+
 public class PropertyInquiryService {
+
+    public PropertyInquiry addInquiry(PropertyInquiry propertyInquiry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addInquiry'");
+    }
+
+    public PropertyInquiry getInquiryById(long inquiryId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInquiryById'");
+    }
+
+    public List<PropertyInquiry> getInquiriesByUser(long userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInquiriesByUser'");
+    }
+
+    public List<PropertyInquiry> getAllInquiries() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllInquiries'");
+    }
+
+    public PropertyInquiry updateInquiryById(long inquiryId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateInquiryById'");
+    }
     
 }
