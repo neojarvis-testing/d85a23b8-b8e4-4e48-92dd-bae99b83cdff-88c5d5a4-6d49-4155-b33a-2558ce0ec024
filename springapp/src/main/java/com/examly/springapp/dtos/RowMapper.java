f8f607211ce6032eef5fb29d3f3260c551eabb89
Deepanshu1008 <deepanshudp10@gmail.com>
2025-04-26 09:09:46 +0000

    Final feedback

--- springapp/src/main/java/com/examly/springapp/dtos/RowMapper.java
+++ springapp/src/main/java/com/examly/springapp/dtos/RowMapper.java
@@ -1,5 +1,7 @@
 package com.examly.springapp.dtos;
 
+import com.examly.springapp.model.Feedback;
+import com.examly.springapp.model.Property;
 import com.examly.springapp.model.PropertyInquiry;
 import com.examly.springapp.model.User;
 
@@ -13,5 +15,23 @@ public class RowMapper {
     return userDTO;
   }
 
+   public static Feedback mapToFeedbackDTO(FeedbackDTO feedbackDTO) {
+	  Feedback feedback=new Feedback();
+	  feedback.setCategory(feedbackDTO.getCategory());
+	    feedback.setDate(feedbackDTO.getDate()); // Assigns the date from DTO
+	    feedback.setFeedbackId(feedbackDTO.getFeedbackId()); // Uses DTO's ID
+	    feedback.setFeedbackText(feedbackDTO.getFeedbackText()); // Assigns DTO's feedback text
+	    // Fixing User assignment
+	    User user = new User();
+	    user.setUserId(feedbackDTO.getUserId());
+	    feedback.setUser(user);
+
+	    // Fixing Property assignment
+	    Property property = new Property();
+	    property.setPropertyId(feedbackDTO.getPropertyId());
+	    feedback.setProperty(property);
+	  
+	  return feedback;
+  }
 
 }
