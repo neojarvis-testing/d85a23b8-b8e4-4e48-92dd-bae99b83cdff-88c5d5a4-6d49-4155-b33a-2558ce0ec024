525fa1b5ccefc20dd434b031e6f94ca8d92e3d37
jaskaransingh16 <jaskaransinghbasra4@gmail.com>
2025-04-30 04:46:14 +0000

    control panel

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-
-import { AdminAddPropertyComponent } from './components/admin-add-property/admin-add-property.component';
 import { AdminEditPropertyComponent } from './components/admin-edit-property/admin-edit-property.component';
 import { AdminViewPropertyComponent } from './components/admin-view-property/admin-view-property.component';
-import { UserViewPropertiesComponent } from './components/user-view-properties/user-view-properties.component';
-
 import { AdminnavComponent } from './components/adminnav/adminnav.component';
 import { UsernavComponent } from './components/usernav/usernav.component';
 import { MyInquiryComponent } from './components/my-inquiry/my-inquiry.component';
--- angularapp/src/app/components/admin-control-panel/admin-control-panel.component.ts
+++ angularapp/src/app/components/admin-control-panel/admin-control-panel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FeedbackService } from 'src/app/services/feedback.service';
+import { PropertyInquiryService } from 'src/app/services/property-inquery.service';
+import { PropertyService } from 'src/app/services/property.service';
 
 @Component({
   selector: 'app-admin-control-panel',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminControlPanelComponent implements OnInit {
 
-  constructor() { }
+  inquiries:any[]=[]
+
+  allInquiries=this.inquiries.length
+  allFeedbacks:any[]=[]
+  totalProperties:any[]=[]
+  allProperties=this.totalProperties.length
+  unresolvedInquiries:any[]=[]
+  allUnresolvedInquiries=this.unresolvedInquiries.length
+  highPriorityInquiries:any[]=[]
+  allHighPriorityInquiries=this.highPriorityInquiries.length
+
+  getAllPropertyInquiries(){
+    this.service.getAllPropertyInquiry().subscribe((data)=>{
+      this.inquiries=data;
+    })
+  }
+  getAllFeedbacks(){
+    this.feedbackService
+  }
+  
+  getHighPriorityInquiries(){
+    this.highPriorityInquiries=this.inquiries.filter((i)=>i.priority=="High")
+  }
+
+  getUnresolvedInquiries(){
+    this.unresolvedInquiries=this.inquiries.filter((i)=>i.status=="Pending")
+  }
+
+  getAllProperties(){
+    this.propertyService.getAllProperties().subscribe((data)=>{
+      this.totalProperties=data;
+    })
+  }
+  constructor(private service:PropertyInquiryService,private feedbackService:FeedbackService,private propertyService:PropertyService) { }
 
   ngOnInit(): void {
+    this.getAllPropertyInquiries()
+    this.getAllFeedbacks()
+    this.getAllProperties()
   }
 
 }
